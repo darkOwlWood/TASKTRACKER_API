024e43b41ed393fece0204aad93e8c6a5b79ae38
Daniel Reyes <owl@DebianOwl.home.owlhost.com>
2021-04-16 12:28:57 -0500

    AUTH_SERVER: fixing bcrypt dependency.

--- AUTH_SERVER/auth/AuthService.js
+++ AUTH_SERVER/auth/AuthService.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jsonwebtoken = require('jsonwebtoken');
 const { ObjectId } = require('mongodb');
 const { config } = require('../config');
